e45c2eb1565abdcb55060760e8a8b756956d1283
Florian Obernbrger <florian.obernberger@gmail.com>
2022-03-21 21:25:02 +0100

    Add missing tests

--- test/spell_out_numbers_test.dart
+++ test/spell_out_numbers_test.dart
@@ -20,4 +20,9 @@ void main() {
     expect(english.toWord(74565416178),
         "seventy-four billion five hundred sixty-five million four hundred sixteen thousand one hundred seventy-eight");
   });
+  test('tests the english translation with decimals', () {
+    final LanguageNumberScheme english = EnglishNumberScheme();
+    expect(english.toWord(10.0), "ten");
+    expect(english.toWord(10.001), "ten and one thousandth");
+  });
 }
